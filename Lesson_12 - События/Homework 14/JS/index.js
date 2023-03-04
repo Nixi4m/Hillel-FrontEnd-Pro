0aa27ed9063e4d6dcc973dbb45ac7bd292f2fbc4
Nixi4m <stetsenko2002@gmail.com>
2023-03-04 19:24:27 +0200

    rename

--- Lesson_12 - События/Homework 14/JS/index.js
+++ Lesson_12 - События/Homework 14/JS/index.js
@@ -1,18 +1,43 @@
-linkChange.addEventListener('click',() => {
-    location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-})
-
-let link = 'http://www.youtube.com/watch?v=dQw4w9WgXcQ'
-
-function httpsCheck (link) {
-    let linkSplit = link.split(':')
-    if (linkSplit[0] == 'http'){
-        linkSplit.shift()
-        linkSplit.unshift('https:')
-        console.log(linkSplit.join(''))
-    }else{
-        console.log(`И так сойдёт ${link}`)
-    }   
+let array = [1, 2, [3.1, 3.2, 3.3], 4, 5, [6.1, 6.2, 6.3], 7, 8];
+ 
+listGenerator(array);
+ 
+function listGenerator(arr) {
+    let total = "<ul>";
+    for (let i = 0; i < arr.length; i++) {
+        if (!Array.isArray(arr[i])) {
+            total += `<li>${arr[i]}</li>`;
+        }else{
+            total += "<li><ul>";
+            for (let j = 0; j < arr[i].length; j++) {
+                total += `<li>${arr[i][j]}</li>`;
+            }
+            total += "</ul></li>";
+        }
+    }
+    total += "</ul>";
+    pablo.innerHTML = total;
 }
-
-httpsCheck(link)
+ 
+listGenerator2(array);
+ 
+function listGenerator2(arr) {
+    let mainUl = document.createElement("ul");
+    for (let i = 0; i < arr.length; i++) {
+        let li = document.createElement("li")
+        if (!Array.isArray(arr[i])) {
+            mainUl.append(li)
+            li.textContent = arr[i]
+        }else{
+            let ul = document.createElement("ul")
+            li.append(ul)
+            for (let j = 0; j < arr[i].length; j++) {
+                let subLi = document.createElement("li")
+                ul.append(subLi)
+                subLi.textContent = arr[i][j]
+            }
+            mainUl.append(li)
+        }
+    }
+    pablo2.append(mainUl)
+}
