56c90f2be6bfbed7140568e037eabe552b5e0a5c
Nixi4m <stetsenko2002@gmail.com>
2023-03-04 19:37:23 +0200

    never gonan give u up

--- Lesson_12 - События/Homework 13/JS/index.js
+++ Lesson_12 - События/Homework 13/JS/index.js
@@ -2,6 +2,10 @@ linkChange.addEventListener('click',() => {
     location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 })
 
+linkChange2.addEventListener('click',() => {
+    location.href='https://www.youtube.com/watch?v=GtL1huin9EE'
+})
+
 let link = 'http://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 function httpsCheck (link) {
