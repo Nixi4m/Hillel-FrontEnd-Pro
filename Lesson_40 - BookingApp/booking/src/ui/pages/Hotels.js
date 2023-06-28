ce86e378db7bc995583c0a9391822ff7bdd23259
Nixi4m <stetsenko2002@gmail.com>
2023-06-28 23:54:36 +0300

    Hotel page rework

--- Lesson_40 - BookingApp/booking/src/ui/pages/Hotels.js
+++ Lesson_40 - BookingApp/booking/src/ui/pages/Hotels.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Grid from '@mui/material/Grid';
+import hotelImage from './images.jpg';
 
 export default function Hotels() {
   const [hotelList, setHotelList] = useState([]);
@@ -17,16 +22,29 @@ export default function Hotels() {
 
     fetchHotels();
   }, []);
+
   return (
     <div>
       <h2>Hotels</h2>
       {/* Отображение списка отелей */}
-      {hotelList.map((hotel) => (
-        <div key={hotel.id}>
-          <h3>{hotel.label}</h3>
-          {/* Остальная информация о каждом отеле */}
-        </div>
-      ))}
+      <Grid container spacing={2} justifyContent="center">
+        {hotelList.map((hotel) => (
+          <Grid item xs={4} key={hotel.id}>
+            <Card sx={{ width: 200, border: '1px solid black' }}>
+              <CardMedia
+                component="img"
+                height="150"
+                image={hotelImage}
+                alt="Hotel"
+              />
+              <CardContent>
+                <h3>{hotel.label}</h3>
+                <p>Score: {hotel.value}</p>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 }
